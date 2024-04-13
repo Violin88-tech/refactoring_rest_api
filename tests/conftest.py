00568922a550224ec26a8790ecae3a953a5dff7d
Viktoria <48157182+violin88@users.noreply.github.com>
2024-04-13 18:38:42 +0400

    add allure

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selene import browser
+
+DOMAIN_URL = 'https://demowebshop.tricentis.com/'
+LOGIN = "example1200@example.com"
+PASSWORD = "123456"
+
+@pytest.fixture(scope='function', autouse=True)
+def open_browser():
+    browser.config.base_url = DOMAIN_URL
+    browser.config.window_width = '1900'
+    browser.config.window_height = '1080'
+    yield browser
+    browser.quit()
---
+++ tests/test_ui_and_api.py
@@ -0,0 +1,40 @@
+from tests.conftest import LOGIN,PASSWORD,DOMAIN_URL
+import requests
+import allure
+from selene import browser, have
+
+
+def test_add_product():
+    with allure. step("Get user cookie"):
+        response = requests.post(DOMAIN_URL + "/login",
+                                 data={"Email": LOGIN, "Password": PASSWORD},
+                                 allow_redirects=False)
+        cookie = response.cookies.get("NOPCOMMERCE.AUTH")
+        browser.open('')
+        browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+
+    with allure.step("Add product to user's cart"):
+        response = requests.post(DOMAIN_URL + 'addproducttocart/catalog/31/1/1',
+                                     cookies={"NOPCOMMERCE.AUTH": cookie})
+
+    with allure.step("Check items in cart"):
+        browser.open('cart')
+        browser.element('.product-unit-price').should(have.text('1590.00'))
+
+    with allure.step("Delete items"):
+        browser.open('cart')
+        browser.element('[name="removefromcart"]').click()
+        browser.element('[name="updatecart"]').click()
+        browser.element('.order-summary-content').should(have.text('Your Shopping Cart is empty!'))
+
+
+
+
+
+
+
+
+
+
+
+
